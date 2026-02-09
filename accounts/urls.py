33a4c29ad445312e5de96d84f6ff3fc6f29dba26
Som24-bkend <shido2407@gmail.com>
2026-02-09 10:29:52 +0530

    Fix accounts register and Profile APIs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, RegisterView
 
 
 urlpatterns = [
+
+    path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,23 @@ from rest_framework.response import Response
 
 
 from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import status
 
 # Create your views here.
+
+class RegisterView(APIView):
+    def post(self,request):
+        username=request.data.get('username')
+        password=request.data.get('password')
+        email=request.data.get('email')
+        if not username or not password :
+            return Response({'error':'Username and password are required'},status=400)
+        
+        if User.objects.filter(username=username).exists():
+            return Response({'error':'Username already exists'},status=400) 
+        user = User.objects.create_user(username=username,password=password,email=email)
+        return Response({'message':'User registered successfully'},status=201)
 
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
